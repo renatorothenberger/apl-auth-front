2b7c0ccbb2ab25193b3245e693ef2d3d7320f7aa
Raphael Melo de Lima <raphaelmelolima@gmail.com>
2020-06-28 21:11:29 -0300

    initial structure of the registration and login page

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+      <h1>Login</h1>
+      <input class="input" type="text" placeholder="Email">
+      <input class="input" type="password" placeholder="Senha">
+      <div class="btn-login">
+          <button>Login</button>
+      </div>
+      <p>Você não tem conta?
+          <a href="/registrar">crie uma conta</a>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {};
+    },
+    methods: {}
+};
+</script>
+
+<style scoped>
+    .input{
+        height: 2rem;
+        margin: 5%;
+    }
+</style>
---
+++ src/views/Registrar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="registra">
+      <h1>Crie uma conta</h1>
+    <input class="input" type="text" placeholder="Nome completo">
+    <input class="input" type="text" placeholder="Email">
+    <input class="input" type="password" placeholder="Senha">
+    <input class="input" type="password" placeholder="Repita a senha">
+
+    <div class="btn-cadastrar">
+        <button>Cadastrar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Registrar',
+    data() {
+        return {};
+    },
+    methods: {}
+};
+</script>
+
+<style scoped>
+    .input{
+        height: 2rem;
+        margin: 5%;
+    }
+</style>
