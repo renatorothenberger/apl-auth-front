5b217d20ff0b1a15450bb95b47c370cb7647d251
Raphael Melo de Lima <raphaelmelolima@gmail.com>
2020-06-28 23:51:02 -0300

    make request with axios

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,69 @@
 <template>
   <div class="login">
       <h1>Login</h1>
-      <input class="input" type="text" placeholder="Email">
-      <input class="input" type="password" placeholder="Senha">
+      <div class=inputs>
+        <input class="input" type="text" placeholder="Email" v-model="email">
+        <input class="input" type="password" placeholder="Senha" v-model="password">
+      </div>
+      <div class="message">{{ info }}</div>
       <div class="btn-login">
-          <button>Login</button>
+          <button @click="login">Login</button>
       </div>
       <p>Você não tem conta?
-          <a href="/registrar">crie uma conta</a>
+          <router-link to="/registrar"> crie uma conta</router-link>
       </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'Login',
     data() {
-        return {};
+        return {
+            info: ''
+        };
     },
-    methods: {}
+    methods: {
+        login() {
+            
+            return axios.get('http://localhost:8000/api/auth', {
+                params: {
+                    email: this.email,
+                    password:  this.password
+                }
+            },{
+                headers: {
+                    'Content-type': 'application/json',
+                }
+            }).then((response) => {
+                if (response.data.token) {
+                    this.$router.replace('/')
+                } else {
+                    this.info = response.data
+
+                }
+            }).catch(error => {
+                console.log({
+                    error: error
+                });
+            });
+        }
+    }
 };
 </script>
 
 <style scoped>
-    .input{
+    input{
         height: 2rem;
-        margin: 5%;
+        margin: 2%;
+    }
+    .inputs{ 
+        display: flex;
+        justify-content: center;
+    }
+    .message{
+        margin: 2%;
     }
 </style>
--- src/views/Registrar.vue
+++ src/views/Registrar.vue
@@ -1,30 +1,73 @@
 <template>
   <div class="registra">
       <h1>Crie uma conta</h1>
-    <input class="input" type="text" placeholder="Nome completo">
-    <input class="input" type="text" placeholder="Email">
-    <input class="input" type="password" placeholder="Senha">
-    <input class="input" type="password" placeholder="Repita a senha">
+      <div class='inputs'>
+        <input class="input" type="text" placeholder="Nome completo" v-model="name">
+        <input class="input" type="text" placeholder="Email" v-model="email">
+        <input class="input" type="password" placeholder="Senha" v-model="password">
+        <input class="input" type="password" placeholder="Repita a senha" v-model="checkPassword">
+      </div>
+
+    <div class="message">{{ info }}</div>
 
     <div class="btn-cadastrar">
-        <button>Cadastrar</button>
+        <button @click="registrar()">Registrar</button>
     </div>
+    <p>
+        ou retorne ao
+        <router-link to="/login"> login</router-link>
+    </p>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'Registrar',
     data() {
-        return {};
+        return {
+            info: ''
+        };
     },
-    methods: {}
+    methods: {
+        registrar() {
+            if(this.password === this.checkPassword) {
+                return axios.post('http://localhost:8000/api/create-user',{
+                    name: this.name,
+                    email: this.email,
+                    password:  this.password
+                },{
+                    headers: {
+                        'Content-type': 'application/json',
+                    }
+                }).then((response) => {
+                    this.info = response.data
+                }
+                ).catch(error => {
+                    console.log({
+                        error: error
+                    });
+                });
+            } else {
+                this.info = 'As senhas digitadas não coincidem'
+            }
+        }
+    }
 };
 </script>
 
 <style scoped>
-    .input{
+    input{
         height: 2rem;
-        margin: 5%;
+        margin: 2%;
+    }
+    .inputs{
+        display: flex;
+        justify-content: center;
+    }
+    .message{
+        margin: 2%;
     }
 </style>
